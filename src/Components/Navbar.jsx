903f9111e47795524e25fd7ac2cbda395a8412ea
Jayant Verma <iamvermajayant@gmail.com>
2023-09-08 20:48:37 +0530

    wrting styled component in

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,31 @@
+import { styled } from "styled-components";
 
+const Container = styled.div`
+`;
+
+const Wrapper = styled.div`
+`;
+
+const Button = styled.button`
+    padding : 5px 15px;
+    background-color : transparent;
+    border : 1px solid #3ea6ff;
+    color : #3ea6ff;
+    border-radius : 3px;
+    font-weight : 300;
+    cursor : pointer;
+    display : flex;
+    align-items : center;
+    gap : 5px;
+    margin-top : 0.5rem;
+`;
+
+const Input = styled.input`
+`;
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>Navbar</Container>
   )
 }
 
