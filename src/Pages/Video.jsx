3f4b0838be4eb952d0ea8e8341004551dfd46c02
Jayant Verma <iamvermajayant@gmail.com>
2023-09-14 20:10:06 +0530

    making title section of video

--- src/Pages/Video.jsx
+++ src/Pages/Video.jsx
@@ -1,6 +1,6 @@
 import { ThumbDownAltOutlined, ThumbUpAltOutlined } from "@mui/icons-material";
-import ReplyOutlinedIcon from '@mui/icons-material/ReplyOutlined';
-import AddTaskOutlinedIcon from '@mui/icons-material/AddTaskOutlined';
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import AddTaskOutlinedIcon from "@mui/icons-material/AddTaskOutlined";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -15,70 +15,81 @@ const Content = styled.div`
 const VideoWrapper = styled.div``;
 
 const Title = styled.h1`
-  font-size : 18px;
-  font-weight : 400;
-  margin-top : 20px;
-  margin-bottom : 10px;
-  color : ${({theme}) => theme.text};
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.text};
 `;
 
 const Details = styled.div`
-  display : flex;
-  align-items : center;
-  justify-content : space-between;
-
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const Info = styled.span`
-  color : ${({theme}) => theme.textSoft};
+  color: ${({ theme }) => theme.textSoft};
 `;
 
 const Buttons = styled.div`
-  display : flex;
-  color : ${({theme}) => theme.text};
-  gap : 20px;
-
+  display: flex;
+  color: ${({ theme }) => theme.text};
+  gap: 20px;
 `;
 
 const Button = styled.div`
-  display : flex;
-  align-items : center;
-  gap : 5px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
 `;
 
 const Channel = styled.div`
-  display : flex;
-  justify-content : space-between;
-
+  display: flex;
+  justify-content: space-between;
 `;
 
 const ChannelInfo = styled.div`
-
+  display : flex;
+  gap : 20px;
 `;
 
 const Subscribe = styled.button`
-
+  background-color : #cc1a00;
+  font-weight : 500;
+  color : white;
+  border : none;
+  border-radius : 3px;
+  height : max-content;
 `;
 
 const Image = styled.img`
-
+  width : 50px;
+  height : 50px;
+  border-radius : 50%;
 `;
 const ChannelDetail = styled.div`
-
+  display : flex;
+  flex-direction : column;
+  color : ${({theme}) => theme.text};
 `;
 const ChannelName = styled.span`
+  font-weight : 400;
 
 `;
 const ChannelCounter = styled.span`
-
+  margin-top : 5px;
+  margin-bottom : 20px;
+  color : ${({theme}) => theme.textSoft};
+  font-size : 12px;
 `;
 const Description = styled.p`
-
+  font-size : 14px;
 `;
 
 const Hr = styled.hr`
-  margin : 15px 0px;
-  border : 0.5px solid ${({theme}) => theme.soft};
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
 `;
 
 const Recommendation = styled.div`
@@ -89,36 +100,58 @@ const Video = () => {
   return (
     <Container>
       <Content>
-      <VideoWrapper>
-        <iframe
-          width="100%"
-          height="720"
-          src="https://www.youtube.com/embed/mZQH8CPQ-wo?si=V6uZw0Ah9ktp3EX7"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-          frameBorder="0"
-        ></iframe>
-      </VideoWrapper>
-      <Title>With You</Title>
-      <Details>
-        <Info>660,998 Views ● 1 Day ago</Info>
-        <Buttons>
-          <Button>
-            <ThumbUpAltOutlined/> 123
-          </Button>
-          <Button><ThumbDownAltOutlined/>Dislike</Button>
-          <Button><ReplyOutlinedIcon/>Share</Button>
-          <Button><AddTaskOutlinedIcon/>Save</Button>
-        </Buttons>
-      </Details>
-      <Hr/>
-      <Channel>
-        <ChannelInfo>
-        
-        </ChannelInfo>
-        <Subscribe>SUBSCRIBE</Subscribe>
-      </Channel>
+        <VideoWrapper>
+          <iframe
+            width="100%"
+            height="720"
+            src="https://www.youtube.com/embed/mZQH8CPQ-wo?si=V6uZw0Ah9ktp3EX7"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            frameBorder="0"
+          ></iframe>
+        </VideoWrapper>
+        <Title>With You</Title>
+        <Details>
+          <Info>660,998 Views ● 1 Day ago</Info>
+          <Buttons>
+            <Button>
+              <ThumbUpAltOutlined /> 123
+            </Button>
+            <Button>
+              <ThumbDownAltOutlined />
+              Dislike
+            </Button>
+            <Button>
+              <ReplyOutlinedIcon />
+              Share
+            </Button>
+            <Button>
+              <AddTaskOutlinedIcon />
+              Save
+            </Button>
+          </Buttons>
+        </Details>
+        <Hr />
+        <Channel>
+          <ChannelInfo>
+            <Image src="https://yt3.googleusercontent.com/ytc/AOPolaSAz3nhUeo5D7ps5MvLjQsCeqdLlB7Yz-MotQLeJA=s176-c-k-c0x00ffffff-no-rj"></Image>
+          </ChannelInfo>
+          <ChannelDetail>
+            <ChannelName>A.P Dhillon</ChannelName>
+            <ChannelCounter>200k Subscribers</ChannelCounter>
+            <Description>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
+              dolores.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Maxime, dolores.Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Maxime, dolores.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Maxime, dolores.Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Maxime, dolores.Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Maxime, dolores.
+            </Description>
+          </ChannelDetail>
+          <Subscribe>SUBSCRIBE</Subscribe>
+        </Channel>
       </Content>
       <Recommendation>Recommendation</Recommendation>
     </Container>
