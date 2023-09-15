7416d8db0bc9fd2d5584399d371a8fe215180773
Jayant Verma <iamvermajayant@gmail.com>
2023-09-15 19:43:44 +0530

    modified Comment.jsx

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -6,8 +6,6 @@ const Container = styled.div`
   margin: 30px 0px;
 `;
 
-const NewComment = styled.div``;
-
 const Avatar = styled.img`
   width: 50px;
   height: 50px;
@@ -18,18 +16,29 @@ const Details = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
+  color : ${({theme})=> theme.text};
 `;
 
-const Name = styled.span``;
+const Name = styled.span`
+    font-size : 13px;
+    font-weight : 500;
+`;
 
-const Date = styled.span``;
+const Date = styled.span`
+    font-size : 12px;
+    font-weight : 400;
+    color : ${({theme}) => theme.textSoft};
+    margin-left : 5px;
+`;
 
-const Text = styled.span``;
+const Text = styled.span`
+    font-size : 14px;
+`;
 
 const Comment = () => {
   return (
     <Container>
-      <Avatar src="" />
+      <Avatar src="https://yt3.googleusercontent.com/DhVq3438WZYaIVF1nsWiR801dCOvfpUn_RkX0goc1FFH-1rzHW_mVl0R1oD1mTz8McdOlPT1Qg=s176-c-k-c0x00ffffff-no-rj-mo" />
       <Details>
         <Name>
           Shinda Kahlon <Date>2 days ago</Date>
